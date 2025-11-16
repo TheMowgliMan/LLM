b141f0b25c329dd4259d2121a8a368d0d9d5e200
Kkidslogin <96443213+TheMowgliMan@users.noreply.github.com>
2025-11-16 09:31:41 -0500

    Update memory.py

--- memory.py
+++ memory.py
@@ -2,3 +2,9 @@ class Memory:
     items = []
     simular_ref = []
     connection_ref = []
+
+    def __getitem__(self, idx):
+        return self.items[idx]
+    
+    def __setitem__(self, idx, datum):
+        self.items[idx] = datum
