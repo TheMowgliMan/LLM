b3b3a0fa6b1497c1e33ed8bf4120209c7fd118bc
Kkidslogin <96443213+TheMowgliMan@users.noreply.github.com>
2025-11-16 08:32:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ class Neuron:
     wei = []
     out = 0
     __lastOut__ = 0
+    __tolerance__ = 0.4
+    __fired__ = False
 
     def __init__(self, inputs, weights = []):
         if not isinstance(inputs, list):
@@ -21,4 +23,30 @@ class Neuron:
         else:
             self.wei = weights
 
-    def cycle
+    def run(self, inputs = []):
+        if len(inputs) != len(inputs) and inputs != []:
+            raise ValueError("Length of inputs must be zero or match the number of defined inputs")
+        
+        self.__fired__ = False
+        # If there are no inputs, generate the proper number of input data to use
+        if inputs != []: self.inp = inputs
+        else: self.inp = [0] * len(self.inp)
+
+        # Process neuron, which is tanh(inp[0] * wei[0], inp[1] * wei[1], [...] inp[x] * wei[x])
+        proc = 0
+        for i in range(len(self.inp)): proc += self.inp[i] * self.wei [i]
+        proc = tanh(proc)
+        # When the neuron is less than the tolerance + part of out, don't fire
+        if abs(proc + self.out / 2) < self.__tolerance__: proc = 0; self.out *= 0.80 # Out decays over time if it fired
+        else: self.out = proc; self.__lastOut__ = proc; self.__fired__ = True # Fire!
+
+        if proc == 0: self.__tolerance__ *= 1.0001 # Long disuse causes tolerance to increase
+
+        # It becomes desensitized to an input if it is far higher than any others
+        getsorted = sorted(self.inp, reverse=True)
+        if abs(getsorted[0]) - abs(getsorted[1]) > 0.3:
+            self.wei[self.inp.index(max(self.inp))] *= 0.9999 # Reduce the weight
+            
+
+        return self.out
+
